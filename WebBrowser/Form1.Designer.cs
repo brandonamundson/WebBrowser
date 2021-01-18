2da1261637c9ccb9c75f26a1c64ffa9f78007146
Brandon Amundson <brandonamundson@users.noreply.github.com>
2021-01-18 12:09:22 -0700

    Add project files.

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System.Windows.Forms;
+using WebBrowser.Properties;
+
+namespace WebBrowser
+{
+    partial class WebBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.browserDisplay = new System.Windows.Forms.WebBrowser();
+			this.goToWeb = new System.Windows.Forms.Button();
+			this.urlBox = new System.Windows.Forms.TextBox();
+			this.homeButton = new System.Windows.Forms.Button();
+			this.backButton = new System.Windows.Forms.Button();
+			this.forwardButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// browserDisplay
+			// 
+			this.browserDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.browserDisplay.Location = new System.Drawing.Point(15, 126);
+			this.browserDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.browserDisplay.MinimumSize = new System.Drawing.Size(20, 20);
+			this.browserDisplay.Name = "browserDisplay";
+			this.browserDisplay.Size = new System.Drawing.Size(1215, 633);
+			this.browserDisplay.TabIndex = 0;
+			this.browserDisplay.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.browserDisplay_Navigated);
+			// 
+			// goToWeb
+			// 
+			this.goToWeb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.goToWeb.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.goToWeb.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.goToWeb.Location = new System.Drawing.Point(1160, 34);
+			this.goToWeb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.goToWeb.Name = "goToWeb";
+			this.goToWeb.Size = new System.Drawing.Size(69, 63);
+			this.goToWeb.TabIndex = 1;
+			this.goToWeb.Text = "Go";
+			this.goToWeb.UseVisualStyleBackColor = false;
+			this.goToWeb.Click += new System.EventHandler(this.goToWeb_Click);
+			// 
+			// urlBox
+			// 
+			this.urlBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.urlBox.BackColor = System.Drawing.SystemColors.Window;
+			this.urlBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.urlBox.Location = new System.Drawing.Point(344, 48);
+			this.urlBox.Margin = new System.Windows.Forms.Padding(4);
+			this.urlBox.Name = "urlBox";
+			this.urlBox.Size = new System.Drawing.Size(808, 36);
+			this.urlBox.TabIndex = 2;
+			// 
+			// homeButton
+			// 
+			this.homeButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.homeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.homeButton.Location = new System.Drawing.Point(8, 36);
+			this.homeButton.Margin = new System.Windows.Forms.Padding(4);
+			this.homeButton.Name = "homeButton";
+			this.homeButton.Size = new System.Drawing.Size(100, 63);
+			this.homeButton.TabIndex = 3;
+			this.homeButton.Text = "Home";
+			this.homeButton.UseVisualStyleBackColor = false;
+			this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+			// 
+			// backButton
+			// 
+			this.backButton.BackColor = System.Drawing.Color.DarkSlateGray;
+			this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.backButton.Location = new System.Drawing.Point(116, 36);
+			this.backButton.Margin = new System.Windows.Forms.Padding(4);
+			this.backButton.Name = "backButton";
+			this.backButton.Size = new System.Drawing.Size(85, 63);
+			this.backButton.TabIndex = 4;
+			this.backButton.Text = "Back";
+			this.backButton.UseVisualStyleBackColor = false;
+			this.backButton.Click += new System.EventHandler(this.backButton_Click);
+			// 
+			// forwardButton
+			// 
+			this.forwardButton.BackColor = System.Drawing.Color.DarkSlateGray;
+			this.forwardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.forwardButton.Location = new System.Drawing.Point(209, 36);
+			this.forwardButton.Margin = new System.Windows.Forms.Padding(4);
+			this.forwardButton.Name = "forwardButton";
+			this.forwardButton.Size = new System.Drawing.Size(127, 64);
+			this.forwardButton.TabIndex = 5;
+			this.forwardButton.Text = "Forward";
+			this.forwardButton.UseVisualStyleBackColor = false;
+			this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+			// 
+			// WebBrowser
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::WebBrowser.Properties.Resources.spider_webs;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(1244, 772);
+			this.Controls.Add(this.forwardButton);
+			this.Controls.Add(this.backButton);
+			this.Controls.Add(this.homeButton);
+			this.Controls.Add(this.urlBox);
+			this.Controls.Add(this.goToWeb);
+			this.Controls.Add(this.browserDisplay);
+			this.Icon = global::WebBrowser.Properties.Resources.web;
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "WebBrowser";
+			this.Text = "Web Browser";
+			this.Load += new System.EventHandler(this.WebBrowser_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser browserDisplay;
+		private System.Windows.Forms.Button goToWeb;
+		private System.Windows.Forms.TextBox urlBox;
+		private System.Windows.Forms.Button homeButton;
+		private System.Windows.Forms.Button backButton;
+		private System.Windows.Forms.Button forwardButton;
+	}
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class WebBrowser : Form
+    {
+	    //sets the homepage as duckduckgo.com search engine
+	    string homepage = "https://duckduckgo.com";
+
+
+	    /// <summary>
+	    /// initializer function for the form.  Pre-created, some components added
+	    /// </summary>
+	    public WebBrowser()
+         {
+	         InitializeComponent();
+		    //surppresses javascript errors from appearing.
+	         browserDisplay.ScriptErrorsSuppressed = true;
+
+		    //adds event handlers letting user know if they are able to continue using back/forward buttons
+	         browserDisplay.CanGoBackChanged += browserDisplay_CanGoBackChanged;
+	         browserDisplay.CanGoForwardChanged += browserDisplay_CanGoForwardChanged;
+
+		    //adds key event handler allowing enter key to press button
+	         urlBox.KeyDown += goToWeb_KeyDown;
+         }
+
+	    /// <summary>
+	    /// function to navigate to web address entered once go button is clicked.
+	    /// </summary>
+	    private void goToWeb_Click(object sender, EventArgs e)
+	    {
+		    //if nothing entered, do nothing
+		    if (string.IsNullOrEmpty(urlBox.Text)) 
+			    return;
+		    
+		    //if address is about:blank, do nothing
+		    if (urlBox.Text.Equals("about:blank"))
+				return;
+		    
+		    //if address is missing http or https prefix, add it and then go
+		    if (!urlBox.Text.StartsWith("http://") &&
+			    !urlBox.Text.StartsWith("https://"))
+		    {
+				urlBox.Text = "http://" + urlBox.Text;
+		    }
+
+		    //attempt to navigate, if it fails catch the exception but do nothing.
+		    try
+		    { 
+			    browserDisplay.Navigate(new Uri(urlBox.Text));
+		    }
+		    catch (System.UriFormatException)
+		    {
+				return;
+		    }
+	    }
+
+	    /// <summary>
+	    /// set the url box and navigate to the url.
+	    /// </summary>
+	    private void homeButton_Click(object sender, EventArgs e)
+	    {
+			urlBox.Text = homepage;
+			browserDisplay.Navigate(homepage);
+	    }
+
+	    /// <summary>
+	    /// upon form load, automatically go to homepage
+	    /// </summary>
+	    private void WebBrowser_Load(object sender, EventArgs e)
+	    {
+			urlBox.Text = homepage;
+			browserDisplay.Navigate(homepage);
+	    }
+
+	    /// <summary>
+	    /// if navigating through by clicking links, update the url text box.
+	    /// </summary>
+	    private void browserDisplay_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+	    { 
+		    urlBox.Text = browserDisplay.Url.ToString();
+	    }
+
+	    /// <summary>
+	    /// if we can go back, enable button; if we cant, disable the button.
+	    /// </summary>
+	    private void browserDisplay_CanGoBackChanged(object sender,
+			EventArgs e)
+	    {
+		    backButton.Enabled = browserDisplay.CanGoBack;
+
+		    backButton.BackColor = !backButton.Enabled ? System.Drawing.Color.DarkSlateGray : System.Drawing.SystemColors.ActiveCaption;
+	    }
+
+	    /// <summary>
+	    /// to go back, use the browser display go back feature
+	    /// </summary>
+	    private void backButton_Click(object sender, EventArgs e)
+	    {
+			browserDisplay.GoBack();
+	    }
+
+	    /// <summary>
+	    /// to go forward, use the browser display forward feature
+	    /// </summary>
+	    private void forwardButton_Click(object sender, EventArgs e)
+	    {
+			browserDisplay.GoForward();
+	    }
+
+	    /// <summary>
+	    /// if we can go forward, enable button; if we cant, disable the button.
+	    /// </summary>
+	    private void browserDisplay_CanGoForwardChanged(object sender,
+			EventArgs e)
+	    {
+		    forwardButton.Enabled = browserDisplay.CanGoForward;
+
+		    forwardButton.BackColor = !forwardButton.Enabled ? System.Drawing.Color.DarkSlateGray : System.Drawing.SystemColors.ActiveCaption;
+	    }
+
+	    /// <summary>
+	    /// if in the urlBox and enter key is hit, click button
+	    /// </summary>
+	    private void goToWeb_KeyDown(object sender, KeyEventArgs e)
+	    {
+			//if enter key was pressed, treat as clicking button
+			if (e.KeyCode == Keys.Enter)
+			{
+				goToWeb.PerformClick();
+			}
+	    }
+    }
+}
